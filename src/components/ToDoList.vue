436f80cb73c92d2d4943d33a33e3f36670effc5b
Cassandra <cassandra.dupe@etud.univ-jfc.fr>
2023-02-06 12:21:50 +0100

    Fin du TP

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,6 +2,7 @@
 // -- import de la fonction permettant de déclarer
 //   une variable comme une varianel d'ETAT
 import { reactive } from "vue";
+import {onMounted} from "vue";
 
 // -- les 2 sous composants utilisés
 import ToDoListItem from "./ToDoListItem.vue";
@@ -16,39 +17,90 @@ import Chose from "../Chose";
 const listeC = reactive([]);
 const url = "https://webmmi.iut-tlse3.fr/~pecatte/todos/public/1/todos"; // l’url de la ressource
 
-function getTodos() {
-  const fetchOptions = { method: "GET" }; // les options de l'API fetch
-  // executer la req AJAX
+function getToDos() {
+  const fetchOptions = { method: "GET" };
+
   fetch(url, fetchOptions)
     .then((response) => {
       return response.json();
     })
     .then((dataJSON) => {
-      // dataJSON = les données renvoyées
-      console.log(dataJSON); // ici le traitement des données …
-      // pour affichage dans le navigateur
+
+      console.log(dataJSON);
+
+      listeC.splice(0, listeC.length);
+      dataJSON.forEach((v) => listeC.push(new Chose(v.id, v.libelle, v.fait)))
+
     })
     .catch((error) => console.log(error));
 }
 
 onMounted(() => {
-  getTodos();
+  getToDos();
 });
 
+
+
 // -- handler pour 'faire/défaire' une chose à prtir de l'index dans la liste
-function handlerFaire(idx) {
-  listeC[idx].faire();
+function handlerFaire(ch) {
+  console.log(ch);
+  ch.faire();
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const fetchOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: JSON.stringify(ch),
+  };
+  fetch(url, fetchOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((dataJSON) => {
+
+        console.log(dataJSON);
+        getToDos();
+      })
 }
+
+
 // -- handle pour supprimer une chose à prtir de l'index dans la liste
 function handlerDelete(idx) {
-  listeC.splice(idx, 1);
+  const fetchOptions = {method: "DELETE"};
+  const urld = url + "/" + listeC[idx].id;
+
+  fetch(urld, fetchOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((dataJSON) => {
+
+        console.log(dataJSON);
+        getToDos();
+      })
 }
 // -- handler pour ajouter une nouvelle chose à partir
 //    du libelle saisi dans le formulaire
 //     qui se retrouve en paramétre
 function handlerAdd(libelle) {
-  // -- il faut créer une nouvelle "chsoe" instance de la classe
-  listeC.push(new Chose(libelle));
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const fetchOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify({ libelle : libelle }),
+  };
+  fetch(url, fetchOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((dataJSON) => {
+
+        console.log(dataJSON);
+        getToDos();
+      })
 }
 </script>
 
@@ -71,7 +123,7 @@ function handlerAdd(libelle) {
       v-for="(chose, index) of listeC"
       :key="chose.id"
       :chose="chose"
-      :indexc="index"
+      :indexc="chose.id"
       @deletec="handlerDelete"
       @fairec="handlerFaire"
     />
